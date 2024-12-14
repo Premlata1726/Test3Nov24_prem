3dba6d5fb714a7d22eb709f78ef92a1ebc6b5c9e
Prem Lata Gupta <premlata.gupta5@gmail.com>
2024-12-14 10:37:17 +0530

    Assignment2-prem

--- src/Assignment23.java
+++ src/Assignment23.java
@@ -2,42 +2,7 @@ package basic;
 
 public class Assignment23 {
 	
-	void printChar(String str) {
-		int uCount=0;
-		int lCount=0;
-		int dCount=0;
-		int sCount=0;
-		int spCount=0;
-		
-		for(int i=0; i<=str.length()-1; i++) {
-			char ch =str.charAt(i);
-			if(Character.isLetter(ch)){
-				if(Character.isUpperCase(ch)){
-				uCount++;
-                }
-			    if(Character.isLowerCase(ch)){
-				lCount++;
-			    }
-			}
-			else if(Character.isDigit(ch)){
-				dCount++;
-			}
-			else if(Character.isWhitespace(ch)){
-				sCount++;
-			}else {
-				spCount++;
-			}
-		}	
-		System.out.println("Uppercase :"+ uCount);
-		System.out.println("Lowercase :"+ lCount);
-		System.out.println("Digit :"+ dCount);
-		System.out.println("Space :"+ sCount);
-		System.out.println("SpecialCharacter :"+ spCount);
-	}
-	
 	public static void main(String[] args) {
-		Assignment23 assign23 =new Assignment23();
-		assign23.printChar("Te4c_hno Credi3ts");
+		System.out.println("Hello_class2");
 	}
-
 }
--- src/Assignment24.java
+++ src/Assignment24.java
@@ -2,46 +2,7 @@ package basic;
 
 public class Assignment24 {
 		
-	void printChar(String str) {
-		int uCount=0;
-		int lCount=0;
-		int dCount=0;
-		int sCount=0;
-		int spCount=0;
-			
-		for(int i=0; i<=str.length()-1; i++) {
-			char ch =str.charAt(i);
-			if(Character.isLetter(ch)){
-				if(Character.isUpperCase(ch)){
-				uCount++;
-				System.out.println(ch);
-                }
-			    if(Character.isLowerCase(ch)){
-				lCount++;
-				System.out.println(ch);
-			    }
-			}
-			else if(Character.isDigit(ch)){
-				dCount++;
-				System.out.println(ch);
-			}
-			else if(Character.isWhitespace(ch)){
-				sCount++;
-				System.out.println(ch);
-			}else {
-				spCount++;
-				System.out.println(ch);
-			}
-		}	
-		System.out.println("Uppercase :"+ uCount);
-		System.out.println("Lowercase :"+ lCount);
-		System.out.println("Digit :"+ dCount);
-		System.out.println("Space :"+ sCount);
-		System.out.println("SpecialCharacter :"+ spCount);
-	}
-		
 	public static void main(String[] args) {
-		Assignment24 assign23 =new Assignment24();
-		assign23.printChar("Te4c_hno Credi3ts");
+		System.out.println("Hello_class1");
 	}
 }
