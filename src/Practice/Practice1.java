5bddbeb137d2bc6f74d6b48e1934aa55ad5fe110
Prem Lata Gupta <premlata.gupta5@gmail.com>
2024-12-14 10:56:07 +0530

    added

---
+++ src/Practice/Practice1.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class Practice1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello_class2");
+		
+	}
+}
---
+++ src/Practice/Practice2.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class Practice2 {
+		
+	public static void main(String[] args) {
+		System.out.println("Hello_class1");
+		
+	}
+}
